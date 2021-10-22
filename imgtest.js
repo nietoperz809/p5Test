0dc2c9ab2a80182bc417a8df23a2d6bb77b7d9ae
Administrator <pbull809@gmail.com>
2021-10-23 00:17:45 +0200

    backup

--- imgtest.js
+++ imgtest.js
@@ -1,22 +1,18 @@
 let img = [];
 let imgDeck;
 let imgBlack;
-let imgs = 15;
 let board = [];
-let visible = [];
 let s_ohoh;
 let s_applause;
 let userHits = 0;
 let browserHits = 0;
 
 function preload() {
-    for (let s = 0; s < imgs; s++) {
+    for (let s = 0; s < 15; s++) {
         let name = 'res/idb' + s + '.bmp'
         img[s] = loadImage(name);
-        board[s] = s;
-        visible[s] = true;
+        board.push ({pos: s, visible: true})
     }
-    visible = visible.concat(visible);
     board = shuffle(board.concat(board));
     imgDeck = loadImage('res/deck.bmp');
     imgBlack = loadImage('res/black.bmp');
@@ -59,10 +55,11 @@ function mouseClicked() {
     if (mouX > 5 || mouY > 4)
         return;
     let idx = mouX + 6 * mouY;
+    if (board[idx].visible === false)
+        return;
     let xpos = mouX * 41;
     let ypos = mouY * 63;
-    console.log(idx, mouX, mouY);
-    image(img[board[idx]], xpos, ypos);
+    image(img[board[idx].pos], xpos, ypos);
     let obj = {x: xpos, y: ypos, idx: idx};
     stack.push(obj);
     clicked++;
@@ -70,19 +67,19 @@ function mouseClicked() {
         clicked = 0;
         let ob = stack[0];
         if (idx !== ob.idx) {
-            if (board[idx] !== board[ob.idx])
+            if (board[idx].pos !== board[ob.idx].pos)
                 s_ohoh.play();
             else {
                 s_applause.play();
                 userHits++;
-                visible[idx] = false;
-                visible[ob.idx] = false;
+                board[idx].visible = false;
+                board[ob.idx].visible = false;
             }
         }
         let timer = setTimeout(function () {
             while (stack.length !== 0) {
                 let ob = stack.pop();
-                if (visible[ob.idx] === true)
+                if (board[ob.idx].visible === true)
                     image(imgDeck, ob.x, ob.y);
                 else
                     image(imgBlack, ob.x, ob.y);
