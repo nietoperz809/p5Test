107b415b50250e694a825ca79055d180f37938c6
Administrator <pbull809@gmail.com>
2021-10-22 23:46:21 +0200

    backup

--- imgtest.js
+++ imgtest.js
@@ -6,6 +6,8 @@ let board = [];
 let visible = [];
 let s_ohoh;
 let s_applause;
+let userHits = 0;
+let browserHits = 0;
 
 function preload() {
     for (let s = 0; s < imgs; s++) {
@@ -24,18 +26,28 @@ function preload() {
 
 function draw_board() {
     background(0);
-    for (let n = 0; n < 5; n++) {
-        for (let s = 0; s < 6; s++) {
-            let idx = n + 6 * s;
-            if (visible[idx] === true)
-                image(imgDeck, s * 41, n * 63);
+    for (let n = 0; n < 6; n++) {
+        for (let s = 0; s < 5; s++) {
+            image(imgDeck, n * 41, s * 63);
+            //let idx = n + 6 * s;
+            //image(img[board[idx]], n * 41, s * 63);
         }
     }
 }
 
 function setup() {
-    createCanvas(800, 400);
+    createCanvas(800, 600);
     draw_board();
+}
+
+function printScore() {
+    fill(255, 0, 0);
+    rect (6, 320, 160, 40);
+    rect (6, 366, 160, 40);
+    textSize(32);
+    fill(255, 204, 0);
+    text('User: ' + userHits, 10, 350);
+    text('Comp: ' + browserHits, 10, 396);
 }
 
 let clicked = 0;
@@ -44,10 +56,12 @@ let stack = [];
 function mouseClicked() {
     let mouX = Math.floor(mouseX / 41);
     let mouY = Math.floor(mouseY / 63);
-    let idx = mouX + 5 * mouY;
+    if (mouX > 5 || mouY > 4)
+        return;
+    let idx = mouX + 6 * mouY;
     let xpos = mouX * 41;
     let ypos = mouY * 63;
-    //console.log(idx, xpos, ypos);
+    console.log(idx, mouX, mouY);
     image(img[board[idx]], xpos, ypos);
     let obj = {x: xpos, y: ypos, idx: idx};
     stack.push(obj);
@@ -60,6 +74,7 @@ function mouseClicked() {
                 s_ohoh.play();
             else {
                 s_applause.play();
+                userHits++;
                 visible[idx] = false;
                 visible[ob.idx] = false;
             }
@@ -72,10 +87,10 @@ function mouseClicked() {
                 else
                     image(imgBlack, ob.x, ob.y);
             }
+            printScore();
             clearTimeout(timer);
         }, 1000);
     }
-    //draw_board();
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
